a7d44b7742bcb399c8c12f47d85f66fc91e44268
STARTIM <msg.stark2@extaccount.com>
2020-11-05 09:31:01 +0100

    also export ui5Steps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ui5Action, ui5ActionDef } from "./ui5Action";
 import { ui5Assert } from "./ui5Asserts";
 
-import { ui5, UI5ChainSelection, UI5ObjectAttributeSelection, UI5TableRowChainSelection, UI5ListChainSelection, UI5ComboBoxChainSelection, UI5CoreItemSelection, UI5BaseBuilder } from "./ui5Builder";
+import { ui5, ui5Steps, UI5ChainSelection, UI5ObjectAttributeSelection, UI5TableRowChainSelection, UI5ListChainSelection, UI5ComboBoxChainSelection, UI5CoreItemSelection, UI5BaseBuilder } from "./ui5Builder";
 import { ui5Test } from "./ui5Test";
-export { ui5, ui5Assert, ui5Test, ui5ActionDef, UI5ChainSelection, UI5ObjectAttributeSelection, UI5TableRowChainSelection, UI5ListChainSelection, UI5ComboBoxChainSelection, UI5BaseBuilder, UI5CoreItemSelection, ui5Action }
+export { ui5, ui5Assert,  ui5Steps, ui5Test, ui5ActionDef, UI5ChainSelection, UI5ObjectAttributeSelection, UI5TableRowChainSelection, UI5ListChainSelection, UI5ComboBoxChainSelection, UI5BaseBuilder, UI5CoreItemSelection, ui5Action }
