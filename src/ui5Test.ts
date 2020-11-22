55b1888b944bdfbea51a4852476cd02813c89672
STARTIM <msg.stark2@extaccount.com>
2020-11-22 10:31:53 +0100

    make it bold

--- src/ui5Test.ts
+++ src/ui5Test.ts
@@ -1,16 +1,17 @@
 import { ui5ActionDef } from "./ui5Action"
+var colors = require('colors/safe');
 
 export function ui5Test(name: string, testCase: string, func: (test: TestController) => Promise<void>) {
     return test
         .after(async t => {
             var sTime = Math.round(((process.uptime()) + Number.EPSILON) * 100) / 100;
-            console.log(testCase + ":" + name + " stopped after " + sTime + "s");
+            console.log(colors.bold(testCase + ":" + name + " stopped after " + sTime + "s"));
         }).meta('TEST_CASE', testCase)(name, async t => {
             t.ctx.name = name;
             t.ctx.testCase = testCase;
             ui5ActionDef.currentTestRun = t;
             var sTime = Math.round(((process.uptime()) + Number.EPSILON) * 100) / 100;
-            console.log(testCase + ":" + name + " started after " + sTime + "s");
+            console.log(colors.bold(testCase + ":" + name + " started after " + sTime + "s"));
             await func(t);
         });
 }
