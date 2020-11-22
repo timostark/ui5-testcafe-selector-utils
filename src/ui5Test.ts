cd088e6100d1d9f1caf71f1ba9c9c1304471db3d
STARTIM <msg.stark2@extaccount.com>
2020-11-22 12:56:52 +0100

    remove console logs for js exceptions

--- src/ui5Test.ts
+++ src/ui5Test.ts
@@ -2,16 +2,20 @@ import { ui5ActionDef } from "./ui5Action"
 var colors = require('colors/safe');
 
 export function ui5Test(name: string, testCase: string, func: (test: TestController) => Promise<void>) {
-    return test
-        .after(async t => {
-            var sTime = Math.round(((process.uptime()) + Number.EPSILON) * 100) / 100;
-            console.log(colors.bold(testCase + ":" + name + " stopped after " + sTime + "s"));
-        }).meta('TEST_CASE', testCase)(name, async t => {
-            t.ctx.name = name;
-            t.ctx.testCase = testCase;
-            ui5ActionDef.currentTestRun = t;
-            var sTime = Math.round(((process.uptime()) + Number.EPSILON) * 100) / 100;
-            console.log(colors.bold(testCase + ":" + name + " started after " + sTime + "s"));
-            await func(t);
-        });
+    return test.clientScripts({
+        content: `
+            window.addEventListener('error', function (e) {
+                alert(e.message); 
+            });`
+    }).after(async t => {
+        var sTime = Math.round(((process.uptime()) + Number.EPSILON) * 100) / 100;
+        console.log(colors.bold(testCase + ":" + name + " stopped after " + sTime + "s"));
+    }).meta('TEST_CASE', testCase)(name, async t => {
+        t.ctx.name = name;
+        t.ctx.testCase = testCase;
+        ui5ActionDef.currentTestRun = t;
+        var sTime = Math.round(((process.uptime()) + Number.EPSILON) * 100) / 100;
+        console.log(colors.bold(testCase + ":" + name + " started after " + sTime + "s"));
+        await func(t);
+    });
 }
